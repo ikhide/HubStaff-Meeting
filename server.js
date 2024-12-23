339156f72162a817eb013096538b54cad30facdf
Godson Atakpu <ikhide.atakpu@gmail.com>
2024-12-24 00:26:29 +0100

    [feat]: Batch process meetings

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const http = require("http");
 const moment = require("moment");
-const mongoose = require("mongoose");
 
 const { PORT, NODE_ENV } = process.env;
 
@@ -19,11 +18,6 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
 app.use((req, res, next) => express.json({ limit: "50mb" })(req, res, next));
 app.use(bodyParser.text({ limit: "50mb" }));
 app.use(cookieParser());
-
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("mongodb connected"))
-  .catch((err) => console.log(err));
 
 // listen to connections
 server.listen(PORT);
